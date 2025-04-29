14b3fc285a94767fefe94fbea67bf655c2294ac9
Uday_Prabhas <udayprabhas005@gmail.com>
2025-04-29 22:00:37 +0530

    jhkd

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@
 export default function Header() {
     return (
       <div className="bg-white px-6 py-4 shadow text-gray-700 font-semibold">
-        Welcome! You are logged in
+        Welcome,You're logged in!!
       </div>
     );
   }
