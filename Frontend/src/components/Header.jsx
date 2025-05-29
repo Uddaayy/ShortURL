d0b3444d656e425de58214cdab1e457699c3ed79
Uday_Prabhas <udayprabhas005@gmail.com>
2025-05-29 12:32:20 +0530

    hello

--- Frontend/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-//Header//
-export default function Header() {
-    return (
-      <div className="bg-white px-6 py-4 shadow text-gray-700 font-semibold">
-        Welcome,You're logged in!!
-      </div>
-    );
-  }
-  
